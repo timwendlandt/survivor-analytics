3be520d5dae705a5d1cf2572f8684f264f0a3ff9
Tim <timwendlandt@gmail.com>
2025-04-02 07:35:00 -0400

    updating dim_contestants query

--- database-files/dim-fact-table-creation.sql
+++ database-files/dim-fact-table-creation.sql
@@ -1,17 +1,38 @@
-﻿USE survivor_data
-
-DROP TABLE dbo.dim_contestants
+﻿USE survivor_data;
 
 /*dim_contestant creation*/
-SELECT DISTINCT contestant_name,
-	hometown,
-	profession,
+WITH hometown_changes AS (
+	SELECT contestant_name,
+		hometown,
+		profession,
+		state_from,
+		country,
+		birthdate,
+		gender,
+		num_season AS effective_season,
+		LAG(hometown) OVER (PARTITION BY contestant_name ORDER BY num_season) AS prior_hometown,
+		FIRST_VALUE(hometown) OVER (PARTITION BY contestant_name ORDER BY num_season DESC) AS current_hometown,
+		LAG(profession) OVER (PARTITION BY contestant_name ORDER BY num_season) AS prior_profession,
+		FIRST_VALUE(profession) OVER (PARTITION BY contestant_name ORDER BY num_season DESC) AS current_profession
+	FROM dbo.contestants)
+
+SELECT contestant_name,
 	state_from,
 	country,
 	birthdate,
-	gender
+	gender,
+	hometown,
+	profession,
+	effective_season,
+	CASE
+		WHEN current_hometown = hometown AND current_profession = profession
+		THEN 1
+		ELSE 0
+	END AS is_current_ind
 INTO dim_contestants
-FROM dbo.contestants
+FROM hometown_changes
+WHERE (hometown <> prior_hometown OR prior_hometown IS NULL) OR (profession <> prior_profession OR prior_profession IS NULL)
+ORDER BY contestant_name
 
 /*Add a surrogate primary key (contestant_name, num_season)*/
 ALTER TABLE dbo.dim_contestants ADD contestant_id INT IDENTITY(1,1)
--- database-files/query-test.sql
+++ database-files/query-test.sql
@@ -3,19 +3,31 @@
 WITH hometown_changes AS (
 	SELECT contestant_name,
 		hometown,
+		profession,
+		state_from,
+		country,
+		birthdate,
+		gender,
 		num_season AS effective_season,
 		LAG(hometown) OVER (PARTITION BY contestant_name ORDER BY num_season) AS prior_hometown,
-		MAX(hometown) OVER (PARTITION BY contestant_name) AS current_hometown
+		FIRST_VALUE(hometown) OVER (PARTITION BY contestant_name ORDER BY num_season DESC) AS current_hometown,
+		LAG(profession) OVER (PARTITION BY contestant_name ORDER BY num_season) AS prior_profession,
+		FIRST_VALUE(profession) OVER (PARTITION BY contestant_name ORDER BY num_season DESC) AS current_profession
 	FROM dbo.contestants)
 
 SELECT contestant_name,
+	state_from,
+	country,
+	birthdate,
+	gender,
 	hometown,
+	profession,
 	effective_season,
 	CASE
-		WHEN current_hometown = hometown
+		WHEN current_hometown = hometown AND current_profession = profession
 		THEN 1
 		ELSE 0
-	END AS current_hometown_ind
+	END AS is_current_ind
 FROM hometown_changes
-WHERE hometown <> prior_hometown OR hometown <> NULL
+WHERE ((hometown <> prior_hometown OR prior_hometown IS NULL) OR (profession <> prior_profession OR prior_profession IS NULL)) AND contestant_name = 'Tyson Apostol'
 ORDER BY contestant_name
