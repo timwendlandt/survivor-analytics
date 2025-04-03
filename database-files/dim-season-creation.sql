fac7570f4231b4760e7bd5af89d5b671f8091b7c
Tim <timwendlandt@gmail.com>
2025-04-03 10:14:09 -0400

    adding dim-season-creation

---
+++ database-files/dim-season-creation.sql
@@ -0,0 +1,16 @@
+﻿USE survivor_data;
+
+/*dim_season creation*/
+SELECT DISTINCT winner,
+	merged_tribe,
+	season,
+	num_season,
+	num_season AS season_id
+INTO dim_season
+FROM dbo.season
+
+ALTER TABLE dbo.dim_season ALTER COLUMN season_id INT NOT NULL
+ALTER TABLE dbo.dim_season ADD CONSTRAINT pk_season_id PRIMARY KEY (season_id)
+
+SELECT *
+FROM dbo.dim_season
