4cfca94ecf74b71a1408f511535a1d99f8e2000e
Tim <timwendlandt@gmail.com>
2025-04-03 15:25:38 -0400

    adding dim-contestant-season-creation

---
+++ database-files/fact-contestant-season-creation.sql
@@ -0,0 +1,27 @@
+﻿USE survivor_data;
+
+/*fact_contestant_season creation*/
+SELECT num_season,
+	contestant_id,
+	finish,
+	num_appearance,
+	merge_made,
+	jury,
+	ftc,
+	votes_against,
+	num_boot,
+	quit,
+	evac,
+	ejected,
+	fmc,
+	num_jury_votes,
+	normalized_finish
+INTO fact_contestant_season
+FROM dbo.contestants
+
+ALTER TABLE dbo.fact_contestant_season ADD contestant_season_id INT IDENTITY(1,1)
+ALTER TABLE dbo.fact_contestant_season ADD CONSTRAINT pk_contestant_season_id PRIMARY KEY (contestant_season_id)
+ALTER TABLE dbo.fact_contestant_season ADD CONSTRAINT unique_contestant_season UNIQUE(contestant_id, num_season)
+
+SELECT *
+FROM dbo.fact_contestant_season
--- database-files/query-test.sql
+++ database-files/query-test.sql
@@ -1,33 +1,16 @@
-﻿USE survivor_data;
-
-WITH type_two_dims AS (
-	SELECT contestant_name,
-		hometown,
-		profession,
-		state_from,
-		country,
-		birthdate,
-		gender,
-		num_season AS effective_season,
-		LAG(hometown) OVER (PARTITION BY contestant_name ORDER BY num_season) AS prior_hometown,
-		FIRST_VALUE(hometown) OVER (PARTITION BY contestant_name ORDER BY num_season DESC) AS current_hometown,
-		LAG(profession) OVER (PARTITION BY contestant_name ORDER BY num_season) AS prior_profession,
-		FIRST_VALUE(profession) OVER (PARTITION BY contestant_name ORDER BY num_season DESC) AS current_profession
-	FROM dbo.contestants)
-
-SELECT contestant_name,
-	state_from,
-	country,
-	birthdate,
-	gender,
-	hometown,
-	profession,
-	effective_season,
-	CASE
-		WHEN current_hometown = hometown AND current_profession = profession
-		THEN 1
-		ELSE 0
-	END AS is_current_ind
-FROM type_two_dims
-WHERE ((hometown <> prior_hometown OR prior_hometown IS NULL) OR (profession <> prior_profession OR prior_profession IS NULL)) AND contestant_name = 'Tyson Apostol'
-ORDER BY contestant_name
+﻿SELECT num_season,
+	contestant_id,
+	finish,
+	num_appearance,
+	merge_made,
+	jury,
+	ftc,
+	votes_against,
+	num_boot,
+	quit,
+	evac,
+	ejected,
+	fmc,
+	num_jury_votes,
+	normalized_finish
+FROM dbo.contestants
