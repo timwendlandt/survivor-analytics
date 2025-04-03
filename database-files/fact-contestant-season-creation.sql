3d52a31f0e079844b8c81bc38a4362e7f5eb578c
Tim <timwendlandt@gmail.com>
2025-04-03 15:34:13 -0400

    add fact-season-creation updates

--- database-files/fact-contestant-season-creation.sql
+++ database-files/fact-contestant-season-creation.sql
@@ -1,7 +1,9 @@
 ﻿USE survivor_data;
 
+DROP TABLE dbo.fact_contestant_season
+
 /*fact_contestant_season creation*/
-SELECT num_season,
+SELECT num_season AS season_id,
 	contestant_id,
 	finish,
 	num_appearance,
@@ -21,7 +23,7 @@ FROM dbo.contestants
 
 ALTER TABLE dbo.fact_contestant_season ADD contestant_season_id INT IDENTITY(1,1)
 ALTER TABLE dbo.fact_contestant_season ADD CONSTRAINT pk_contestant_season_id PRIMARY KEY (contestant_season_id)
-ALTER TABLE dbo.fact_contestant_season ADD CONSTRAINT unique_contestant_season UNIQUE(contestant_id, num_season)
+ALTER TABLE dbo.fact_contestant_season ADD CONSTRAINT unique_contestant_season UNIQUE(contestant_id, season_id)
 
 SELECT *
 FROM dbo.fact_contestant_season
